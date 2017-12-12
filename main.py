cbbee2049b62621b2ef786f791595fee7b506a76
anagalingam <anagalingam95@gmail.com>
2017-12-11 21:49:53 -0500

    Added main and util functions file

--- main.py
+++ main.py
@@ -1,7 +1,45 @@
 import numpy as np
+import utils
 
-trainFile = input("Enter the name of the training data file: ")
-initFile = input("Enter the name of the initialization file: ")
-resFile = input("Enter the name of the trained weights output file: ")
+trainObj = None
+initObj = None
+resFile = None
+num_epochs = 0
+learning_rate = 0
+
+while( trainObj is None ):
+    trainFile = raw_input("Enter the name of the training data file: ")
+    trainObj = utils.parseFile(trainFile, "data")
+    if( trainObj is None ):
+        print "Invalid data file!\n"
+
+while( initObj is None ):
+    initFile = raw_input("Enter the name of the initialization file: ")
+    initObj = utils.parseFile(initFile, "weights")
+    if( initObj is None ):
+        print "Invalid weights file!\n"
+
+while( resFile is None ):
+    resFileInput = raw_input("Enter the name of the trained weights output file: ")
+    if( os.access(resFileInput, os.F_OK||os.W_OK) ):
+        resFile = resFileInput 
+    else:
+        print "Invalid output file path!"
+
+while( num_epochs < 1 ):
+    try:
+        num_epochs = int(raw_input("Enter the number of epochs for training: "))
+    except ValueError:
+        pass
+    if( num_epochs < 1 ):
+        print "Invalid number of epochs! Must be a positive integer."
+
+while( learning_rate <= 0 ):
+    try:
+        learning_rate = float(raw_input("Enter the learning rate for training: "))
+    except ValueError:
+        pass
+    if( learning_rate <= 0 ):
+        print "Invalid learning rate! Must be a positive float (decimal notation)"
 
 
---
+++ utils.py
@@ -0,0 +1,42 @@
+# Function to parse the input files
+import numpy as np
+
+def parseFile( fileName , fileType ):
+    if( fileType != "data" and fileType != "weights" ):
+        return
+    try:
+        fileObj = open( fileName , 'r')
+    except IOError:
+        return
+    firstLine = np.array(fileObj.readline().strip('\n').split(' '))
+    fileParams = firstLine.astype(np.int)
+    if( fileType == "data" ):
+        features = np.zeros( (fileParams[0], fileParams[1]) )
+        targets = np.zeros( (fileParams[0], fileParams[2]) )
+        try:
+            for lineNum in range(0, fileParams[0]):
+                thisLine = fileObj.readline().strip('\n').split(' ')
+                features[lineNum] = thisLine[0:-1]
+                targets[lineNum] = thisLine[-1]
+        except ValueError:
+            return
+        return fileParams, features, targets
+    
+    # else fileType == weights
+    weights_hidden = np.zeros( (fileParams[1],fileParams[0]+1) )
+    weights_out = np.zeros( (fileParams[2],fileParams[1]+1) )
+    try:
+        for lineNum in range(0,fileParams[1]):
+            weights_hidden[lineNum] = fileObj.readline().strip('\n').split(' ')
+        for lineNum in range(0, fileParams[2]):
+            weights_out[lineNum] = fileObj.readline().strip('\n').split(' ')
+    except ValueError:
+        return
+    return fileParams, weights_hidden, weights_out
+
+def sigmoid( x ):
+    return 1/(1+np.exp(-x))
+
+def sigmoidPrime( x ):
+    return sigmoid(x) * (1 - sigmoid(x))
+
